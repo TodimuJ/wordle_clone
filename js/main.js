35fbd6301b3af3c84eec03804088f0b61446268f
TodimuJ <todimujenrola@gmail.com>
2022-02-06 10:30:46 -0500

    Wordle

--- js/main.js
+++ js/main.js
@@ -2,26 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     buildSquares();
 
-    const guessedWords = [[]]
+    let guessedWords = [[]]; //array to whole each letter of each word guessed
+    let availableSpace = 1; //variable will store position of next letter
 
     const keys = document.querySelectorAll('.keyboard-row button');
 
     for (let index = 0; index < keys.length; index++) {
         keys[index].onclick = ({ target }) => {
-            const key = target.getAttribute("data-key");
+            const letter = target.getAttribute("data-key");
 
-            console.log(key);
+            updateGuessedWords(letter);
         };
     }
 
     function getCurrentWordArr(){
         const numberOfGuessedWords = guessedWords.length;
 
-        return guessedWords[numberOfGuessedWords - 1 ]
+        return guessedWords[numberOfGuessedWords - 1];
     }
 
     function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr()
 
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+
+            const availableSpaceEl = document.getElementById(String(1));
+
+            availableSpace = availableSpace + 1;
+
+            availableSpaceEl.textContent = letter;
+        }
     }
 
     function buildSquares() {
