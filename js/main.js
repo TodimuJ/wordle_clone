7d7747c167655dcdf38696d77c3b4ef4783d180e
TodimuJ <todimujenrola@gmail.com>
2022-02-06 11:02:24 -0500

    Wordle

--- js/main.js
+++ js/main.js
@@ -7,13 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const keys = document.querySelectorAll('.keyboard-row button');
 
-    for (let index = 0; index < keys.length; index++) {
-        keys[index].onclick = ({ target }) => {
-            const letter = target.getAttribute("data-key");
-
-            updateGuessedWords(letter);
-        };
-    }
+    let word = "pizza";
+    let guessedWordCount = 0;
 
     function getCurrentWordArr(){
         const numberOfGuessedWords = guessedWords.length;
@@ -27,13 +22,47 @@ document.addEventListener("DOMContentLoaded", () => {
         if (currentWordArr && currentWordArr.length < 5) {
             currentWordArr.push(letter);
 
-            const availableSpaceEl = document.getElementById(String(1));
+            const availableSpaceEl = document.getElementById(availableSpace);
 
             availableSpace = availableSpace + 1;
 
             availableSpaceEl.textContent = letter;
         }
     }
+
+    function handleSubmitWord() {
+        const currentWordArr = getCurrentWordArr();
+
+        //word has to be equal to 5 letters
+        if (currentWordArr.length !== 5){
+            window.alert("Word has to be 5 letters!");
+        }
+
+        const currentWord = currentWordArr.join('');
+
+
+        const firstLetterId = (guessedWordCount * 5) + 1;
+        const interval = 200;
+
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = "rgb(58, 58, 60)"
+
+            }, interval)
+        })
+
+        //congratulate user on guessing correct word
+        if (currentWord === word) {
+            window.alert("You guessed the right word!");
+        }
+
+        if (guessedWords.length == 6){
+            window.alert(`You have used up all your guesses! The word was ${word}. `);
+        }
+
+        guessedWords.push([]); //add new array to list to move to next line
+    }
+
 
     function buildSquares() {
         const gameBoard = document.getElementById("board")
@@ -43,8 +72,23 @@ document.addEventListener("DOMContentLoaded", () => {
             
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate__animated");
             square.setAttribute("id", index+1);
             gameBoard.appendChild(square);
         }
     }
-})
+
+    for (let index = 0; index < keys.length; index++) {
+        keys[index].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+
+            if (letter === "enter") {
+                handleSubmitWord();
+                return;      
+            }
+
+
+            updateGuessedWords(letter);
+        };
+    }
+});
